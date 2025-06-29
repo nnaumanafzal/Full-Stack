26a95cb36e4f9e5481c90d4eb78f4458d775d250
Nauman Afzal <nauman.afzal@tuni.fi>
2025-06-29 18:33:06 +0300

    filter logic adjusted

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,10 +1,9 @@
 const Persons = ({persons, find}) => {
     return (
         <div>
-            {find === ''
-            ? persons.map(person => <span key={person.id}>{person.name} {person.number}<br/></span>)
-            : persons.filter(p => p.name.toLowerCase().includes(find.toLowerCase()))
-            .map(person => <span key={person.id}>{person.name} {person.number}<br/></span>)
+            {
+                persons.filter(p => p.name.toLowerCase().includes(find.toLowerCase()))
+                    .map(person => <span key={person.id}>{person.name} {person.number}<br/></span>)
             }
         </div>
     )
